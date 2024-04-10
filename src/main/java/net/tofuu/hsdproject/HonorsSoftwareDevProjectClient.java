69f868e1ad9ae8f496608c1ae8234d64edc789b6
Brand <brandino0125@gmail.com>
2024-04-10 00:17:28 -0700

    Added Mod Files Template

---
+++ src/main/java/net/tofuu/hsdproject/HonorsSoftwareDevProjectClient.java
@@ -0,0 +1,11 @@
+package net.tofuu.hsdproject;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HonorsSoftwareDevProjectClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+
+	}
+}
